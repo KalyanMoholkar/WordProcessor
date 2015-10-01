677bdd72178f7d6e7f061768199ef51f25091303
KalyanMoholkar <kalyanmoholkar@gmail.com>
2015-10-01 18:53:18 +0800

    Update UnitTestWordProcessor.cs
    Added one more test method for negative testing

--- UnitTestWordProcessor.cs
+++ UnitTestWordProcessor.cs
@@ -36,5 +36,32 @@ namespace Author
             Assert.IsTrue(expectedDictionary.SequenceEqual(outputDictionary));
               
         }
+        [TestMethod]
+        public void Validate_WordCount_InSentence_NegativeTesting()
+        {
+            //Step1: arrange 
+            //test input
+            var sentence = "This is a statement, and so is this.";
+
+            //expected output            
+            var expectedDictionary = new System.Collections.Generic.Dictionary<string, int> 
+            {
+                {"this",2},
+                {"is", 2},
+                {"a", 1},
+                {"statement", 2},  //changed for negative testing from 1 to 2
+                {"and", 1},
+                {"so",  1 }
+            };
+
+            //Step2: act
+            //Calculate the word count and store in dictionary
+            var outputDictionary = WordProcessor.Calculate_WordCount(sentence);
+
+            // Step3: assert 
+            //compare expected dictionary and actual dictionary derived from sentence
+            Assert.IsFalse(expectedDictionary.SequenceEqual(outputDictionary));
+
+        }
     }
 }
